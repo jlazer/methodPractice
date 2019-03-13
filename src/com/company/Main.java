f71bca32c9fe81680998d7ac86135a77a7469647
Justin Lazarski <blackhawks189@gmail.com>
2019-03-12 19:31:39 -0700

    First commit.

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+// Justin Lazarski
+// CIS3090.04
+// 3/12/19
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        public static void myMethod() {
+            System.out.println("myMethod has been called.");
+
+        }
+    }
+}
