3f101502ac12654069b97e56edcfe4610c4f1f6e
Justin Lazarski <blackhawks189@gmail.com>
2019-03-12 19:51:53 -0700

    Added pizza diameter method. The program now allows the user to input a value that will be used in the method.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,30 @@
 // 3/12/19
 
 package com.company;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // write your code here
+        Scanner input = new Scanner(System.in);
+
         System.out.println("About to encounter a method.");
         currentYear();
+        System.out.println("Please enter a pizza diameter.");
+        printPizzaArea(input.nextInt());
     }
     public static void currentYear() {
         System.out.println("currentYear has been called.");
         int x = 2019;
         System.out.println("The current Year is " + x);
     }
+        public static void printPizzaArea(double pizzaDiameter) {
+            double pizzaRadius;
+            double pizzaArea;
+            double piVal = 3.14159265;
 
-}
+            pizzaRadius = pizzaDiameter / 2.0;
+            pizzaArea = piVal * pizzaRadius * pizzaRadius;
+            System.out.print(pizzaDiameter + " inch pizza is ");
+            System.out.println(pizzaArea + " inches squared.");
+        }
+    }
